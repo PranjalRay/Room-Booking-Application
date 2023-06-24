95aa68ddfbefc9940800a5179096b9fb68568230
PranjalRay <96900065+PranjalRay@users.noreply.github.com>
2023-06-24 11:37:34 +0530

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = function(sequelize) {
+  const User = sequelize.define('User', {
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+
+  return User;
+};
